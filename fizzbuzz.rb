bef0a40ea3eb15a27fd13b0e2aedcc4d31d0c81d
soramugi <soramugi.chika@gmail.com>
2012-06-10 12:51:20 +0900

    昔のfizzbuzzを追加

---
+++ fizzbuzz.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/ruby
+
+1.upto(100) do |i|
+
+    p = i
+    if i % 3 == 0 then
+        p = "Fizz"
+    end
+    if i % 5 == 0 then
+        p = "Buzz"
+    end
+    if i % 15 == 0 then
+        p = "FizzBuzz"
+    end
+
+    print p, ", "
+end
+print "\n"
